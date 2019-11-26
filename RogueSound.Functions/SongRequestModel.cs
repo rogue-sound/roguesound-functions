1e1ad6933097791d7d25fd49f250ba26e83f933e
Pablo Ramos Villaplana <pabravil@gmail.com>
2019-11-26 18:45:28 +0100

    Added SongRequestModel

---
+++ RogueSound.Functions/SongRequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueSound.Functions
+{
+    public class SongRequestModel
+    {
+        public int RoomId { get; set; }
+
+        public string SongId { get; set; }
+
+        public int Duration { get; set; }
+
+        public DateTime ResquestTime { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+    }
+}
