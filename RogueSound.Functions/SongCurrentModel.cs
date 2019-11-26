0a39821cd62cd26decc3ba73959bd62c85af3f77
Pablo Ramos Villaplana <pabravil@gmail.com>
2019-11-26 19:06:16 +0100

    Added SongCurrentModel

---
+++ RogueSound.Functions/SongCurrentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueSound.Functions
+{
+    public class SongCurrentModel
+    {
+        public string SongId { get; set; }
+
+        public int TimerPosition { get; set; }
+    }
+}
