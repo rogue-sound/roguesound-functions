b7fc9a9abff181c96bbc33ffdd4f84d046c35d24
Javi Mollà <javimollamico@gmail.com>
2019-11-26 18:25:29 +0100

    Sample solution

---
+++ RogueSound.Functions/Function1.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Documents.Client;
+
+namespace RogueSound.Functions
+{
+    public static class Function1
+    {
+        private static IConfigurationRoot config = new ConfigurationBuilder()
+        .SetBasePath(Environment.CurrentDirectory)
+        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+
+        private static DocumentClient client = GetCustomClient();
+        private static DocumentClient GetCustomClient()
+        {
+            DocumentClient customClient = new DocumentClient(
+                new Uri(config["CosmosEndpoint"]),
+                config["CosmosKey"],
+                new ConnectionPolicy
+                {
+                    ConnectionMode = ConnectionMode.Direct,
+                    ConnectionProtocol = Protocol.Tcp,
+                    // Customize retry options for Throttled requests
+                    RetryOptions = new RetryOptions()
+                    {
+                        MaxRetryAttemptsOnThrottledRequests = 10,
+                        MaxRetryWaitTimeInSeconds = 30
+                    }
+                });
+
+            return customClient;
+        }
+
+        [FunctionName("GetSong")]
+        public static async Task<IActionResult> GetSong(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        [FunctionName("AddSong")]
+        public static async Task<IActionResult> AddSong(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
