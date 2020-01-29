28188943f5562af0751e56531d5e75a6d7cafa8a
Pablo Ramos Villaplana <pabravil@gmail.com>
2020-01-29 20:22:39 +0100

    Added RemoveSong function WIP with RemoveSongRequestModel

---
+++ RogueSound.Functions/Model/RemoveSongRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueSound.Functions
+{
+    public class RemoveSongRequestModel
+    {
+        public string SongId { get; set; }
+        
+        public string User { get; set; }
+    }
+}
