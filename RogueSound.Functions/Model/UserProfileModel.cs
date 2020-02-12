a32ba88c978bd22a30e56140399c07fb6c02c1d2
jmolla31 <jmolla31@gmail.com>
2020-02-12 23:11:46 +0100

    create base UserProfileModel

---
+++ RogueSound.Functions/Model/UserProfileModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueSound.Functions.Model
+{
+    public class UserProfileModel
+    {
+        public string id { get; set; }
+
+        public string UserIdentifier { get; set; }
+
+        public string CustomName { get; set; }
+
+        public string CustomPicture { get; set; }
+
+        public int SongsPlayed { get; set; }
+    }
+}
